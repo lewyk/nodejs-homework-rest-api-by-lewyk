88ff2edb2d92859e59b561e8395ce3c180440a00
Alexeii Deviatov <lewyk@hotmail.com>
2021-05-10 21:10:46 +0300

    fix upd validation

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const schemaCreateContact = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
+  name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   phone: Joi.number().required()
 });
