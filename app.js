d0d449f815a51a1fd5c924a609d09790dc6e7faf
Alexeii Deviatov <lewyk@hotmail.com>
2021-06-06 19:56:53 +0300

    add coment

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(
 app.use(limiter);
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json({ limit: 15000 }));
+app.use(express.json({ limit: 15000 })); // размер передаваемого body в запросе
 app.use(boolParser());
 
 app.use('/api/users', usersRouter);
