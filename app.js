22b6e9fa9d0b5003542c6acf5a0c2331549457d3
Alexeii Deviatov <lewyk@hotmail.com>
2021-06-16 22:53:47 +0300

    add index page

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(boolParser());
 app.use('/api/users', usersRouter);
 app.use('/api/contacts', contactsRouter);
 
-app.use('/', (req, res) => res.send('<h3>Conracts api</h3>'));
+app.use('/', (req, res) => res.send('<h3>Contacts api</h3>'));
 
 app.use((req, res) => {
   res.status(404).json({ status: 'error', code: 404, message: 'Not found' });
